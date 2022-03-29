b467f21803ca8292ad78916ac548cd2d4e637f72
Doneal Bercier <donealbraxton@gmail.com>
2022-03-29 12:42:47 -0400

    Automatic Search Function
    -predictive text on Scryfall Search

--- scripts/apiCalls.js
+++ scripts/apiCalls.js
@@ -1,4 +1,19 @@
+function keyupFunction() {
+    var cardName = document.getElementById('name').value;
+        var cleanCard = cardName.replace(/\s/g, '+');
+
+        $.getJSON('https://api.scryfall.com/cards/named?fuzzy='+cleanCard, function(a){
+            document.getElementById('artist').innerHTML = "Artist: "+a['artist'];
+            document.getElementById('cardName').innerHTML = "Card Name: "+a['name'];
+            document.getElementById("myImg").src = a['image_uris']['png'];
+         
+
+            });
+
+  }
 $(document).ready(function(){
+
+
 
     $("#search").click(function(){
         
--- scripts/database.js
+++ scripts/database.js
@@ -1,13 +1,45 @@
-function showHint(str) {
-    if (str.length == 0) {
-      document.getElementById("txtHint").innerHTML = "";
-      return;
-    } else {
-      const xmlhttp = new XMLHttpRequest();
-      xmlhttp.onload = function() {
-        document.getElementById("txtHint").innerHTML = this.responseText;
-      }
-    xmlhttp.open("GET", "gethint.php?q=" + str);
-    xmlhttp.send();
-    }
-  }
+$(document).ready(function() {
+                
+                         
+
+    $("#login-form-submit").click(function(){
+   
+        var username = document.getElementById('inputUser').value
+        var pass = document.getElementById('inputPassword').value; 
+        $.ajax({
+            url: 'https://doncards-fa13.restdb.io/rest/decks',
+            type: 'GET',
+            dataType: 'json',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-apikey': '6242758367937c128d7c92b6'
+            },
+            contentType: 'application/json; charset=utf-8',
+            success: function (result) { 
+            
+            
+                alert(username+ pass);
+                if (username === result[0]["username"] && pass === result[0]["password"]) {
+                    alert("You have successfully logged in.");
+            
+                } else {
+                    alert("You have clicked the Sign in Button.");
+                }
+            },
+            error: function (error) {
+                alert("not working");
+            }
+        });
+
+
+            
+    
+      });
+
+
+
+
+
+
+
+});
