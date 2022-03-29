94743569c6081ad786b1bf8ed416b2c2e1274cef
Doneal Bercier <donealbraxton@gmail.com>
2022-03-29 12:51:51 -0400

    Added Timeout Function for Fuzzy Search

--- scripts/apiCalls.js
+++ scripts/apiCalls.js
@@ -1,16 +1,24 @@
+var timeoutId= 0;
 function keyupFunction() {
-    var cardName = document.getElementById('name').value;
-        var cleanCard = cardName.replace(/\s/g, '+');
+    clearTimeout(timeoutId); // doesn't matter if it's 0
+    timeoutId = setTimeout(getFilteredResultCount, 500);
 
-        $.getJSON('https://api.scryfall.com/cards/named?fuzzy='+cleanCard, function(a){
-            document.getElementById('artist').innerHTML = "Artist: "+a['artist'];
-            document.getElementById('cardName').innerHTML = "Card Name: "+a['name'];
-            document.getElementById("myImg").src = a['image_uris']['png'];
-         
-
-            });
 
   }
+
+function getFilteredResultCount(){
+    var cardName = document.getElementById('name').value;
+    var cleanCard = cardName.replace(/\s/g, '+');
+
+    $.getJSON('https://api.scryfall.com/cards/named?fuzzy='+cleanCard, function(a){
+        document.getElementById('artist').innerHTML = "Artist: "+a['artist'];
+        document.getElementById('cardName').innerHTML = "Card Name: "+a['name'];
+        document.getElementById("myImg").src = a['image_uris']['png'];
+     
+
+        });
+}
+
 $(document).ready(function(){
 
 
