09f8a23d95ae4fb88edece3ad20a74959e3baf43
Doneal Bercier <donealbraxton@gmail.com>
2022-04-02 23:45:14 -0400

    Cookie and Popup Login Page

--- scripts/database.js
+++ scripts/database.js
@@ -1,39 +1,37 @@
 $(document).ready(function() {
 
-    $("#login-form").submit(function(e){
-    
-        var x = document.getElementById('inputUser').value
-        var y = document.getElementById('inputPassword').value; 
-        alert("clicked");
-        $.ajax({
-            url: 'https://doncards-fa13.restdb.io/rest/decks',
-            type: 'GET',
-            dataType: 'json',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-apikey': '6242758367937c128d7c92b6'
-            },
-            contentType: 'application/json; charset=utf-8',
-            success: function (result) {             
-            
-                alert(result[0]["username"]);
-                if (x === result[0]["username"] && y === result[0]["password"]) {
-                    alert("You have successfully logged in.");
-            
-                } else {
-                    alert("You have clicked the Sign in Button.");
-                }
-            },
-            error: function (error) {
-                alert("not working");
-            }
-        });
-        e.preventDefault();
-      });
-
 //------------------------------------------------------------------------
 
-
+$("#login-form").submit(function(e){
+    
+    var x = document.getElementById('inputUser').value
+    var y = document.getElementById('inputPassword').value; 
+    alert("clicked");
+    $.ajax({
+        url: 'https://doncards-fa13.restdb.io/rest/decks',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-apikey': '6242758367937c128d7c92b6'
+        },
+        contentType: 'application/json; charset=utf-8',
+        success: function (result) {             
+        
+            alert(result[0]["username"]);
+            if (x === result[0]["username"] && y === result[0]["password"]) {
+                alert("You have successfully logged in.");
+        
+            } else {
+                alert("You have clicked the Sign in Button.");
+            }
+        },
+        error: function (error) {
+            alert("not working");
+        }
+    });
+    e.preventDefault();
+  });
 
 
 
@@ -65,13 +63,21 @@ function setCookie(cname, cvalue, exdays) {
     let user = getCookie("username");
     if (user != "") {
       alert("Welcome again " + user);
+      
+      off();
     } else {
       user = prompt("Please enter your name:", "");
       if (user != "" && user != null) {
         setCookie("username", user, 365);
+        on();
       }
     }
   }
 
-
-
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
