e1558219fc528119d693922e5fff71df03a78b79
Doneal Bercier <donealbraxton@gmail.com>
2022-04-02 22:55:44 -0400

    Check Cookie Load

--- scripts/apiCalls.js
+++ scripts/apiCalls.js
@@ -4,7 +4,7 @@ function keyupFunction() {
     timeoutId = setTimeout(getFilteredResultCount, 500);
 
 
-  }
+}
 
 function getFilteredResultCount(){
     var cardName = document.getElementById('name').value;
@@ -17,6 +17,24 @@ function getFilteredResultCount(){
      
 
         });
+}
+function onmouseover(x){
+    var cardName = x.innerHTML;
+    var cleanCard = cardName.replace(/\s/g, '+');
+
+    $.getJSON('https://api.scryfall.com/cards/named?fuzzy='+cleanCard, function(a){
+        document.getElementById('artist').innerHTML = "Artist: "+a['artist'];
+        document.getElementById('cardName').innerHTML = "Card Name: "+a['name'];
+        document.getElementById("myImg").src = a['image_uris']['png'];
+     
+
+        });
+}
+
+function showcard(x){
+    clearTimeout(timeoutId); // doesn't matter if it's 0
+    timeoutId = setTimeout(onmouseover(x), 500);
+
 }
 
 $(document).ready(function(){
--- scripts/database.js
+++ scripts/database.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-                
-                         
 
-    $("#login-form-submit").click(function(){
-   
-        var username = document.getElementById('inputUser').value
-        var pass = document.getElementById('inputPassword').value; 
+    $("#login-form").submit(function(e){
+    
+        var x = document.getElementById('inputUser').value
+        var y = document.getElementById('inputPassword').value; 
+        alert("clicked");
         $.ajax({
             url: 'https://doncards-fa13.restdb.io/rest/decks',
             type: 'GET',
@@ -15,11 +14,10 @@ $(document).ready(function() {
                 'x-apikey': '6242758367937c128d7c92b6'
             },
             contentType: 'application/json; charset=utf-8',
-            success: function (result) { 
+            success: function (result) {             
             
-            
-                alert(username+ pass);
-                if (username === result[0]["username"] && pass === result[0]["password"]) {
+                alert(result[0]["username"]);
+                if (x === result[0]["username"] && y === result[0]["password"]) {
                     alert("You have successfully logged in.");
             
                 } else {
@@ -30,16 +28,50 @@ $(document).ready(function() {
                 alert("not working");
             }
         });
-
-
-            
-    
+        e.preventDefault();
       });
 
-
+//------------------------------------------------------------------------
 
 
 
 
 
 });
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+      }
+    }
+  }
+
+
+
